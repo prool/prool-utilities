97d263c94644b7c0d809bc5b6b9a99bff8b43107
Prool <proolix@gmail.com>
2018-08-30 11:42:44 +0300

    Былины теперь показывают статистику в первой выдаче телнета, до логина. Соответственно скорректирован скрипт сбора статистики Былин

--- russian-mud-statistics/byl.sh
+++ russian-mud-statistics/byl.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-# script for get online count from Byliny
-echo -n `date` " " >> /tmp2/mudstat.log2
-tt++ scr.byl| tee /tmp/byl1.txt | grep -a видимых | tee /tmp/byl2.txt | ./filtr2|awk '{i=0+$2; if (i>0) i=i-1; print "byl.value " i}'|tee /tmp/byl-stat.tmp| tee -a /tmp2/mudstat.log2
-cp /tmp/byl-stat.tmp /tmp2/byl-stat.txt
+# script for get online count from Byliny, port 4000
+tt++ scr.byl | grep Online | awk '{print "byl.value " $2}' | tee /tmp2/byl-stat.txt
--- russian-mud-statistics/byl5555.sh
+++ russian-mud-statistics/byl5555.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-# script for get online count from Byliny
-echo -n `date` " " >> /tmp2/mudstat.log2
-tt++ scr5555.byl| tee /tmp/byl5555-1.txt | grep -a видимых | tee /tmp/byl5555-2.txt | ./filtr2|awk '{i=0+$2; if (i>0) i=i-1; print "byl5555.value " i}'|tee /tmp/byl5555-stat.tmp| tee -a /tmp2/mudstat.log2
-cp /tmp/byl5555-stat.tmp /tmp2/byl5555-stat.txt
+# script for get online count from Byliny, port 5555
+tt++ scr5555.byl | grep Online | awk '{print "byl5555.value " $2}' | tee /tmp2/byl5555-stat.txt
