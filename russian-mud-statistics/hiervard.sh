d617750aecdaea805e49c108d70f1f0594f1ade9
Prool <proolix@gmail.com>
2019-10-23 17:09:05 +0300

    MUD statistics: fix module for Hiervard MUD

--- russian-mud-statistics/hiervard.sh
+++ russian-mud-statistics/hiervard.sh
@@ -1,2 +1,8 @@
 #/bin/sh
 curl --connect-timeout 120 http://hiervard.ru/info/stat/online.shtml 2>/dev/null | grep -a Смертн  | tail -1 | awk '{print "hiervard.value " $6}' | tee /tmp2/hiervard.txt
+if test -s /tmp2/hiervard.txt
+then
+    echo 
+else
+    echo hiervard.value 0 | tee /tmp2/hiervard.txt
+fi
